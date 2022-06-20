b7bf99e63709f8e65c84303153a14b458aebb627
Arthur Crymble <arthur.crymble@icloud.com>
2022-06-20 23:17:41 +0100

    Add changes to correct outer borders

--- index.js
+++ index.js
@@ -2,16 +2,30 @@
 const container = document.getElementById("container");
 
 function createGrid(gridSize){
-    for (let x = 0; x < gridSize; x++){
+    for (let y = 0; y < gridSize; y++){
         let row = document.createElement("div");
         row.className = "row"
         container.appendChild(row);
     
-        for (let y = 0; y < gridSize; y++){
+        for (let x = 0; x < gridSize; x++){
             let square = document.createElement("div");
             square.className = "square";
             square.style.height = "100%";
             square.style.width = "100%";
+
+            if (x == 0){
+                square.style.borderLeftWidth = "0.05em";
+            } else if (x == gridSize - 1){
+                square.style.borderRightWidth = "0.05em";
+            }
+
+            if (y == 0){
+                square.style.borderTopWidth = "0.05em";
+            } else if (y == gridSize - 1){
+                square.style.borderBottomWidth = "0.05em";
+            }
+
+
             row.appendChild(square);
         }
     }
