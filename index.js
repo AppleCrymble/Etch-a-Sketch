5758f56590f936814b2870bc94303573a288880f
Arthur Crymble <arthur.crymble@icloud.com>
2022-06-20 23:08:36 +0100

    Add createGrid function

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 const container = document.getElementById("container");
 
-for (let x = 0; x < 16; x++){
-    for (let y = 0; y < 16; y++){
-        let square = document.createElement("div");
-        square.className = "gridSquare";
-        container.appendChild(square);
+function createGrid(gridSize){
+    for (let x = 0; x < gridSize; x++){
+        let row = document.createElement("div");
+        row.className = "row"
+        container.appendChild(row);
+    
+        for (let y = 0; y < gridSize; y++){
+            let square = document.createElement("div");
+            square.className = "square";
+            square.style.height = "100%";
+            square.style.width = "100%";
+            row.appendChild(square);
+        }
     }
 }
+
+createGrid(16)
